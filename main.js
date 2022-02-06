ebd56ddc2f7b1a6d90e9ff0e7653fb6a8d4a0e48
raphaelCoelhoDeLima <rcl.lp@hotmail.com>
2022-02-05 21:40:25 -0300

    Update_Aluramidi

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play()
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play()
 }
 
-function tocaSomClap(){
-    document.querySelector('#som_tecla_clap').play()
-}
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+const ListaDeTeclas = document.querySelectorAll('.tecla')
 
+let contador = 0
+
+
+while(contador  < ListaDeTeclas.length){
+    const tecla = ListaDeTeclas[contador]
+    const instrumento = tecla.classList[1]
+    
+    const idAudio = `#som_${instrumento}`
+    
+    tecla.onclick = function (){
+        tocaSom(idAudio)
+    }
+    contador++
+   
+}
